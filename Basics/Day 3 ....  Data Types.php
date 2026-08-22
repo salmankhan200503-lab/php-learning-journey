30022e6b9cb87a2ddcce8c76d5622696a97022c2
salmankhan200503-lab <salmankhan200503@gmail.com>
2026-08-22 17:58:39 +0500

    Add files via upload

---
+++ Basics/Day 3 ....  Data Types.php
@@ -0,0 +1,18 @@
+<?php
+
+
+echo "It's My Day Three of Learning PHP. Our today's topic is Data Types<br><br>";
+
+$name = "Ahmed";       // String
+echo $name . "<br>";
+
+$age = 21;             // Integer
+echo $age . "<br>";
+
+$height = 5.9;         // Float
+echo $height . "<br>";
+
+$isStudent = true;     // Boolean
+echo $isStudent . "<br>";
+
+?>
