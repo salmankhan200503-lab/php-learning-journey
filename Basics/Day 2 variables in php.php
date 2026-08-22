14913480d7263fb4f3c368bcc383e1754e737870
salmankhan200503-lab <salmankhan200503@gmail.com>
2026-08-22 17:32:44 +0500

    Add files via upload

---
+++ Basics/Day 2 variables in php.php
@@ -0,0 +1,20 @@
+<?php
+
+$name ="Salman Khan";
+  echo $name;
+  echo "<br>";
+
+  echo $name;
+  echo "<br>";
+
+  echo $name;
+  echo "<br>";
+  
+  echo $name;
+  echo "<br>";
+
+  echo $name;
+  echo "<br>";
+
+
+?>
